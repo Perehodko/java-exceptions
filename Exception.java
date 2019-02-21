895b37c94afd414f610d082e93a5056d8cdcdf82
Nadya <n.perehodko@yandex.com>
2019-02-21 15:15:38 +0300

    Update Exception.java

--- Exception.java
+++ Exception.java
@@ -1,11 +1,17 @@
-import org.junit.jupiter.api.Test;
+//Перехват и обработка исключения, с использованием конструкции try-catch.
+//Проверяется наличие директории, если она не существует, то создается директория и выводится сообщение 
+//"Каталог new_directory создан. Полный путь к катологу: /home/nper/Desktop/new_directory",
+//если недостаточно прав для доступа к катологу, то выводится сообщение "У Вас нет доступа к этому катологу."
+//Если директория существует, то выводится сообщение "Каталог new_directory по адресу /home/nper/Desktop/new_directory 
+//уже существует".
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 
 public class Exception {
     public static void main(String[] args) throws IOException {
-        String path = "C:\\Users\\Nadya\\Desktop\\new_directory";
+        String path = "/home/nper/Desktop/new_directory";
         File directory = new File(path);
         if (!directory.exists()) {
             try {
